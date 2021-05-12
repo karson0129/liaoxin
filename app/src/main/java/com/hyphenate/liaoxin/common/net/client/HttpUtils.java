d1ac467c7104614d572bdff1da67b7c83c0d902e
karson <k81904791@163.com>
2021-05-12 23:11:39 +0800

    添加网络

--- app/src/main/java/com/hyphenate/liaoxin/common/net/client/HttpUtils.java
+++ app/src/main/java/com/hyphenate/liaoxin/common/net/client/HttpUtils.java
@@ -5,12 +5,15 @@ import android.util.Log;
 import com.hyphenate.liaoxin.common.net.callback.ResultCallBack;
 import com.hyphenate.liaoxin.common.net.interceptor.LoggingInterceptor;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Cache;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
+import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -44,17 +47,33 @@ public class HttpUtils {
 
     private void initClient(){
         client = new OkHttpClient.Builder()
-                .readTimeout(8, TimeUnit.SECONDS)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
                 .addInterceptor(new LoggingInterceptor())
+//                .addInterceptor(new Interceptor() {
+//                    @Override
+//                    public Response intercept(Chain chain) throws IOException {
+//                            Request request = chain.request()
+//                                    .newBuilder()
+//                                    .addHeader("Authorization", "{timestamp:"+ time +",token:'"+ token +"',uid:"+ uid +"}")
+//                                    .addHeader("version","Android/"+ BanbenUtils.getSystemModel()+
+//                                            "/"+BanbenUtils.getVersionCode(CommunityApplication.getContext()))
+//                                    .addHeader("device_id",BanbenUtils.getIMEI(CommunityApplication.getContext()))
+////                                    .addHeader("X_COMAPP_PRODUCTION","0") //测试不传 预发布0 正式1
+//                                    .build();
+////                            Log.i("result", "imei: "+BanbenUtils.getIMEI(CommunityApplication.getContext()));
+//                            return chain.proceed(request);
+//                    }
+//                })
+                .retryOnConnectionFailure(false)
                 .build();
     }
 
-    public void post(String url, ResultCallBack callBack){
-
-        String requestBody = "I am Jdqm.";
+    public void post(String url, String str,ResultCallBack callBack){
         Request request = new Request.Builder()
                 .url(url)
-                .post(RequestBody.create(mediaType, requestBody))
+                .post(RequestBody.create(mediaType, str))
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
